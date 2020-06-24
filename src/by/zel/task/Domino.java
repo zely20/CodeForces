e7a5fdc46d51e6861f80014bfd0bc90e2dedfef2
zely20@mail.ru <Civik1984>
2020-06-24 21:44:05 +0300

    Domino was done

--- src/by/zel/task/Domino.java
+++ src/by/zel/task/Domino.java
@@ -1,4 +1,18 @@
 package by.zel.task;
 
+import java.util.Scanner;
+
 public class Domino {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+        int result = 0;
+        result = n*m/2;
+
+        System.out.println(result);
+
+    }
 }
