40f277bd19246f1b45d7b7f964b38c10f65405c2
zely20@mail.ru <Civik1984>
2020-06-24 21:41:51 +0300

    add Domino

---
+++ src/by/zel/task/Domino.java
@@ -0,0 +1,4 @@
+package by.zel.task;
+
+public class Domino {
+}
--- src/by/zel/task/Team.java
+++ src/by/zel/task/Team.java
@@ -1,4 +1,32 @@
 package by.zel.task;
 
+import java.util.Scanner;
+
 public class Team {
+
+    public static void main(String[] args) {
+        int count;
+        int result = 0;
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[][] array = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 3; j++) {
+                array[i][j] = scan.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            count = 0;
+            for (int j = 0; j < 3; j++) {
+               if (array[i][j] == 1) {
+                   count++;
+               }
+            }
+            if (count > 1) {
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
 }
