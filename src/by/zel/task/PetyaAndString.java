4cff9676dac899ac70fd3644f1f741b44d84c524
Aliaksandr Zelianko <zely20@mail.ru>
2020-06-25 16:15:58 +0300

    PetyaAndString was done

--- src/by/zel/task/PetyaAndString.java
+++ src/by/zel/task/PetyaAndString.java
@@ -9,8 +9,16 @@ public class PetyaAndString {
         String one = scan.next();
         String two = scan.next();
         int result = 0;
-        Character.getNumericValue(one.charAt(0));
+
         for (int i = 0; i < one.length(); i++) {
+            if(Character.getNumericValue(one.charAt(i)) < Character.getNumericValue(two.charAt(i))){
+                result = -1;
+                break;
+            }else if(Character.getNumericValue(one.charAt(i)) > Character.getNumericValue(two.charAt(i))) {
+                result = 1;
+                break;
+            }
         }
+        System.out.println(result);
     }
 }
