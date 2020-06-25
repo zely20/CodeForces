6eb9e8cbdb2335959884b95855384574a8a770d7
Aliaksandr Zelianko <zely20@mail.ru>
2020-06-25 14:41:42 +0300

    PetyaAndString

---
+++ src/by/zel/task/PetyaAndString.java
@@ -0,0 +1,16 @@
+package by.zel.task;
+
+import java.util.Scanner;
+
+public class PetyaAndString {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String one = scan.next();
+        String two = scan.next();
+        int result = 0;
+        Character.getNumericValue(one.charAt(0));
+        for (int i = 0; i < one.length(); i++) {
+        }
+    }
+}
